5b4e2a12d26ee72e46cd48b3319eafc9aede1c44
estergcarvalho <ester.estudosti@gmail.com>
2025-11-30 21:16:57 -0300

    fix: Adicionando classe de paginacao

---
+++ Application/ViewModels/PaginacaoParams.cs
@@ -0,0 +1,8 @@
+namespace TeamHeartFiap.ViewModels
+{
+    public class PaginacaoParams
+    {
+        public int Page { get; set; } = 1;     
+        public int PageSize { get; set; } = 10;
+    }
+}
--- Domain/Candidato.cs
+++ Domain/Candidato.cs
@@ -6,7 +6,7 @@ namespace TeamHeartFiap.Domain
 {
     public class Candidato
     {
-        [Key] // Garante que o Entity Framework reconheça Id como chave primária
+        [Key] 
         public int Id { get; set; }
         
         public string? Nome { get; set; }
@@ -14,10 +14,7 @@ namespace TeamHeartFiap.Domain
         public DateTime DataCandidatura { get; set; } = DateTime.UtcNow;
         public string? Demografico { get; set; }
         public bool PrioridadeDiversidade { get; set; } = false;
-
-        // ** CORREÇÃO CRÍTICA **
-        // Esta coleção é necessária para resolver o símbolo 'ConclusoesTreinamento' 
-        // no AppDbContext, que mapeia a relação com ConclusaoTreinamento.
+        
         public ICollection<ConclusaoTreinamento> ConclusoesTreinamento { get; set; } = new List<ConclusaoTreinamento>();
     }
 }
